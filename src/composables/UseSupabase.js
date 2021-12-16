6357181b6626e8122cd3e1964ca6ec2c9ff32625
Tom Booster <tom.booster@gmail.com>
2021-12-16 10:23:34 -0800

    Update supabaseUrl and supabaseKey

--- src/composables/UseSupabase.js
+++ src/composables/UseSupabase.js
@@ -5,9 +5,9 @@ import useAuthUser from "@/composables/UseAuthUser";
 // these can come from an environment variable if desired
 // not required however as they are 100% exposed on the client side anyway
 // and that's ok, Supabase expects this
-const supabaseUrl = "https://csybesgskolzayjluqcf.supabase.co";
+const supabaseUrl = "https://tboiwcaxhnefcrgizbfo.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNDM1Mzc1NiwiZXhwIjoxOTQ5OTI5NzU2fQ.W0cD9D_RRAzlGQdIYMsOmpIRDC_1jxqe7c506KiPsCU";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNDg1NTc4MSwiZXhwIjoxOTUwNDMxNzgxfQ.js-GCwqzf-2ShJLScpTIcADu06fQ_drPFldN0vTnWNE";
 
 // setup client
 const supabase = createClient(supabaseUrl, supabaseKey);
